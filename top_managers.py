8b9b5e6cde28e682eaade89634c0557635cbb0c8
Daniel <danielilaro@hotmail.com>
2020-09-13 18:33:11 -0300

    update top_managers.py and add try... except

--- top_managers.py
+++ top_managers.py
@@ -5,7 +5,8 @@ import pandas as pd
 overallLeageID = 314
 
 # number of GW in 2019/20 season. Done as array to avoid calling api for blank GW 30-38.
-gameWeeks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,39,40,41,42,43,44,45,46,47]
+gameWeeks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,39,40,
+			41,42,43,44,45,46,47]
 
 # adds the top 10 team ID's to this array
 teamIDarray = []
@@ -37,8 +38,11 @@ for manager in managerParsed:
 		header = ['rank','entry','player_name','entry_name','total']
 		csvwriter1.writerow(header)
 		count += 1
+	
 	if count <= topManagerNumber:
-		csvwriter1.writerow([manager['rank'],manager['entry'],manager['player_name'],manager['entry_name'],manager['total']])
+		csvwriter1.writerow([manager['rank'],manager['entry'],manager['player_name'],
+							manager['entry_name'],manager['total']])
+
 		count +=1
 		teamIDarray.append(manager['entry'])
 
@@ -54,10 +58,18 @@ for teamID in teamIDarray:
 
 		# write data to top_managers_gwInfo.csv
 		if count1 == 0:
-			header = ['team_id','gw','points','bench','gw_rank','transfers','hits','total_points','overall_ank','team_value','chip']
+			header = ['team_id','gw','points','bench','gw_rank','transfers','hits','total_points',
+					'overall_ank','team_value','chip']
+
 			csvwriter2.writerow(header)
 			count1 += 1
-		csvwriter2.writerow([teamID,x, parsed['entry_history']['points'],parsed['entry_history']['points_on_bench'],parsed['entry_history']['rank'],parsed['entry_history']['event_transfers'],parsed['entry_history']['event_transfers_cost'],parsed['entry_history']['total_points'],parsed['entry_history']['overall_rank'],int(parsed['entry_history']['value'])/10,parsed['active_chip']])
+		try:
+			csvwriter2.writerow([teamID,x, parsed['entry_history']['points'], parsed['entry_history']['points_on_bench'],
+								parsed['entry_history']['rank'], parsed['entry_history']['event_transfers'],
+							 	parsed['entry_history']['event_transfers_cost'], parsed['entry_history']['total_points'],
+							 	parsed['entry_history']['overall_rank'], int(parsed['entry_history']['value'])/10, parsed['active_chip']])
+		except:
+			continue
 
 		# write data to top_managers_gwPicks.csv
 		for i in range(len(parsed['picks'])):
@@ -74,6 +86,7 @@ gw_picks.close()
 # do some formatting on top_managers_gwPicks by adding the name of the player picked from player_idlist.csv
 df = pd.read_csv('data/2019-20/managers/top_managers_gwPicks.csv')
 df1 = pd.read_csv('data/2019-20/player_idlist.csv') 
+
 merged = df.merge(df1, on=['id'])
 merged.drop('first_name', axis=1, inplace=True)
 merged = merged[['team_id', 'gw', 'second_name', 'id', 'position', 'multiplier']]
