aa3b40dad442ac18479ece3c6497d443f80ea673
abhinay-m <m.abhinay@gmail.com>
2020-10-01 15:21:47 +0530

    adding my analysis' python notebooks

---
+++ fpl_eda.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+
+#merged_gw = pd.read_csv('./data/2019-20/gws/merged_gw.csv')
