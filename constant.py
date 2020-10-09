06f3f6ab95ab6cacb0cc8a7a34f37d873199b494
Thomas Kunwar <yathomasi@gmail.com>
2020-10-09 22:36:00 +0545

    added constat seperately

---
+++ constant.py
@@ -0,0 +1,9 @@
+SEASON = "2020-21"
+
+# Overall FPL league ID, 314 for 2019/20 season.
+OVERALL_LEAGUE_ID = 314
+
+# number of GW in 2019/20 season. Done as array to avoid calling api for blank GW 30-38.
+# GAMEWEEKS = [i for i in range(1, 30)] + [i for i in range(39, 48)]
+# for 2020/21
+GAMEWEEKS = [i for i in range(1, 39)]
