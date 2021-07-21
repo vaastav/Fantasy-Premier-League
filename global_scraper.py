0601bb2992bd202000461c7df7f56fc2d2e3d5a1
Vaastav Anand <vaastav.anand05@gmail.com>
2021-07-21 22:57:13 +0200

    Add 2021-22 initial data. Here We Go - Confirmed

--- global_scraper.py
+++ global_scraper.py
@@ -8,7 +8,7 @@ import csv
 def parse_data():
     """ Parse and store all the data
     """
-    season = '2020-21'
+    season = '2021-22'
     base_filename = 'data/' + season + '/'
     print("Getting data")
     data = get_data()
