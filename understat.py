24347f6bffff78816662b6ec89a9002d9a940e85
Vaastav Anand <vaastav.anand05@gmail.com>
2021-09-17 11:22:44 +0200

    Fix understat data

--- understat.py
+++ understat.py
@@ -20,7 +20,7 @@ def get_data(url):
     return scripts
 
 def get_epl_data():
-    scripts = get_data("https://understat.com/league/EPL/2020")
+    scripts = get_data("https://understat.com/league/EPL/2021")
     teamData = {}
     playerData = {}
     for script in scripts:
