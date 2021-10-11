6f72d7c6409ab58f1ffb3dd0484c58bd92a77c4f
Vaastav Anand <vaastav.anand05@gmail.com>
2021-10-11 17:18:28 +0200

    Add id dictionary between understat and fpl

--- understat.py
+++ understat.py
@@ -5,6 +5,7 @@ import re
 import codecs
 import pandas as pd
 import os
+import csv
 
 def get_data(url):
     response = requests.get(url)
@@ -78,11 +79,53 @@ def parse_epl_data(outfile_base):
         player_name = player_name.replace(' ', '_')
         indi_player_frame.to_csv(os.path.join(outfile_base, player_name + '_' + d['id'] + '.csv'), index=False)
 
+class PlayerID:
+    def __init__(self, us_id, fpl_id, us_name, fpl_name):
+        self.us_id = str(us_id)
+        self.fpl_id = str(fpl_id)
+        self.us_name = us_name
+        self.fpl_name = fpl_name
+        
+
+def match_ids(understat_dir, data_dir):
+    with open(os.path.join(understat_dir, 'understat_player.csv')) as understat_file:
+        understat_inf = csv.DictReader(understat_file)
+        ustat_players = {}
+        for row in understat_inf:
+            ustat_players[row['player_name']] = row['id']
+
+    with open(os.path.join(data_dir, 'player_idlist.csv')) as fpl_file:
+        fpl_players = {}
+        fpl_inf = csv.DictReader(fpl_file)
+        for row in fpl_inf:
+            fpl_players[row['first_name'] + ' ' + row['second_name']] = row['id']
+    players = []
+    found = {}
+    for k, v in ustat_players.items():
+        if k in fpl_players:
+            player = PlayerID(v, fpl_players[k], k, k)
+            players += [player]
+            found[k] = True
+        else:
+            player = PlayerID(v, -1, k, "")
+            players += [player]
+
+    for k, v in fpl_players.items():
+        if k not in found:
+            player = PlayerID(-1, v, "", k)
+            players += [player]
+
+    with open(os.path.join(data_dir, 'id_dict.csv'), 'w+') as outf:
+        outf.write('Understat_ID, FPL_ID, Understat_Name, FPL_Name\n')
+        for p in players:
+            outf.write(p.us_id + "," + p.fpl_id + "," + p.us_name + "," + p.fpl_name + "\n")
+
 def main():
-    parse_epl_data('data/2021-22/understat')
+    #parse_epl_data('data/2021-22/understat')
     #md, sd, gd = get_player_data(318)
     #match_frame = pd.DataFrame.from_records(md)
     #match_frame.to_csv('auba.csv', index=False)
+    match_ids('data/2021-22/understat', 'data/2021-22')
 
 if __name__ == '__main__':
     main()
