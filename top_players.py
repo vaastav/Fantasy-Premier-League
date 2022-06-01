ae6fbd51185c8755ce80085ee167c01bb9e1ecfc
Vimal Rajesh <vimrajesh@gmail.com>
2022-06-01 19:19:35 +0530

    Updated Season value to 2021-22.

--- top_players.py
+++ top_players.py
@@ -3,7 +3,7 @@ from parsers import *
 
 def main():
     data = get_data()
-    parse_top_players(data, 'data/2020-21')
+    parse_top_players(data, 'data/2021-22')
 
 if __name__ == '__main__':
     main()
