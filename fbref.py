535da73955ff30539916c7ee1066191d6e70e4df
Vaastav Anand <vaastav.anand05@gmail.com>
2022-07-26 16:15:09 +0200

    Add fbref individual match data

--- fbref.py
+++ fbref.py
@@ -18,6 +18,7 @@ class PlayerData:
         self.base_url = ""
         self.matches_links = []
         self.matches = []
+        self.match_stat_set = set()
 
 def get_data(url):
     response = requests.get(url)
@@ -51,19 +52,26 @@ def get_matches_data(player):
     for l in player.matches_links:
         tables += [get_table_data(l)]
     matches = []
+    match_stat_set = set()
     for t in tables:
         for row in t.tbody.find_all('tr'):
             data = {}
             class_name = row.get('class')
-            if class_name != None and len(class_name) > 0:
+            if class_name != None and len(class_name) > 0 and 'unused_sub' not in class_name:
                 continue
             columns = row.find_all('td') + row.find_all('th')
             for c in columns:
                 data_stat = c.get('data-stat')
-                if data_stat in ['date', 'round', 'comp']:
-                    a_html = BeautifulSoup(str(c.contents[0]), 'html.parser')
-                    a = a_html.find_all('a')
-                    data[data_stat] = a[0].contents[0]
+                match_stat_set.add(data_stat)
+                if data_stat in ['date', 'round', 'comp', 'opponent', 'squad']:
+                    for i in range(len(c.contents)):
+                        a_html = BeautifulSoup(str(c.contents[i]), 'html.parser')
+                        a = a_html.find_all('a')
+                        if len(a) > 0:
+                            if len(a[0].contents) > 0:
+                                data[data_stat] = a[0].contents[0]
+                elif data_stat == 'match_report':
+                    continue
                 else:
                     if len(c.contents) == 0:
                         continue
@@ -72,11 +80,10 @@ def get_matches_data(player):
             match.date = data['date']
             match.round = data['round']
             match.comp = data['comp']
-            del data['date']
-            del data['round']
-            del data['comp']
+            match.data = data
             matches += [match]
     player.matches = matches
+    player.match_stat_set = match_stat_set
 
 def get_epl_players():
     tables = get_data("https://fbref.com/en/comps/9/stats/Premier-League-Stats")
@@ -137,8 +144,13 @@ def get_epl_players():
 def main():
     players, stats = get_epl_players()
 
-    #for id, player in players.items():
-    #    get_matches_data(player)
+    for id, player in players.items():
+        get_matches_data(player)
+        with open('data/2021-22/fbref/' + id + '.csv', 'w') as outf:
+            writer = csv.DictWriter(outf, fieldnames=list(player.match_stat_set))
+            writer.writeheader()
+            for match in player.matches:
+                writer.writerow(match.data)
 
     with open('data/2021-22/fbref_overview.csv', 'w') as outf:
         writer = csv.DictWriter(outf, fieldnames=list(stats))
