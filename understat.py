b2543a40b7df4692ea61dd0169b07c07c77a5bda
Vaastav Anand <vaastav.anand05@gmail.com>
2023-01-13 05:59:17 +0100

    Add DGW19 data

--- understat.py
+++ understat.py
@@ -125,7 +125,7 @@ def main():
     #md, sd, gd = get_player_data(318)
     #match_frame = pd.DataFrame.from_records(md)
     #match_frame.to_csv('auba.csv', index=False)
-    match_ids('data/2021-22/understat', 'data/2022-23')
+    match_ids('data/2022-23/understat', 'data/2022-23')
 
 if __name__ == '__main__':
     main()
